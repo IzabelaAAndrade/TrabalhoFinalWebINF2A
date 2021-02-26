044280bbef00273b7b326cd2957bc34f93adfdbf
victorwo <33638656+victorwo@users.noreply.github.com>
2021-02-26 20:24:10 -0300

    Create conteudos.sql

---
+++ DB_init/sql/E/conteudos.sql
@@ -0,0 +1,8 @@
+CREATE TABLE IF NOT EXISTS conteudos (
+  `id` bigint(20) NOT NULL,
+  `id_etapas` int(11) NOT NULL,
+  `id_disciplinas` int(11) NOT NULL,
+  `conteudos` varchar(30) CHARACTER SET latin1 COLLATE latin1_general_ci NOT NULL,
+  `datas` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+COMMIT;
