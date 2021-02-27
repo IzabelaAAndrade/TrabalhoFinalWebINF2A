8af8603f01afaa3e4ea859b3f97bf52d30259064
MariaECarmona <51420077+MariaECarmona@users.noreply.github.com>
2021-02-27 16:48:01 -0300

    Update etapas.sql

--- DB_init/sql/E/etapas.sql
+++ DB_init/sql/E/etapas.sql
@@ -28,7 +28,7 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE IF NOT EXISTS etapas (
-  `id-ORDEM` int(15) NOT NULL,
+  `id_ORDEM` int(15) NOT NULL,
   `valor` int(5) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 COMMIT;
