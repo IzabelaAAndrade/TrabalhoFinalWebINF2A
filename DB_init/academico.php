8cda12587a898809fb496a6e8cbe1b468d14cb4a
Arthur71149 <51198520+Arthur71149@users.noreply.github.com>
2021-02-27 20:13:48 -0300

    Inclui as tabelas do grupo B no BD

--- DB_init/academico.php
+++ DB_init/academico.php
@@ -4,7 +4,9 @@ include_once 'lib/libConnection.php';
 include_once 'lib/libInitDB.php';
 
 // lista com o caminho dos arquivos sql a serem interpretados
-$sql_list = array();
+$sql_list = array(
+  'sql/B/campi', 'sql/B/cursos', 'sql/B/turmas'
+);
 initDB($conn, $sql_list, 'academico', true);
 
 ?>
