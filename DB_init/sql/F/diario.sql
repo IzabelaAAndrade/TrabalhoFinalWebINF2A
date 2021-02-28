2c57b5261e66803eda3484f44f4c5b1d8aab6f13
Mayara Mendes <mayara.mendes.mdc@gmail.com>
2021-02-28 11:43:58 -0300

    incluindo itens faltantes

---
+++ DB_init/sql/F/diario.sql
@@ -0,0 +1,41 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.3
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Tempo de geração: 28-Fev-2021 às 14:06
+-- Versão do servidor: 10.4.14-MariaDB
+-- versão do PHP: 7.2.34
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Banco de dados: `projeto_academico`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `diario`
+--
+
+CREATE TABLE IF NOT EXISTS `diario` (
+  `id_conteudos` int(11) NOT NULL,
+  `id_matriculas` int(11) NOT NULL,
+  `id_atividades` int(11) NOT NULL,
+  `faltas` int(11) NOT NULL,
+  `nota` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
