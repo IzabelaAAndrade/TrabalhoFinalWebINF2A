d19127884947f1c534160f1ed907c80fbce294b4
Vinicius Lima <52431971+viniilima@users.noreply.github.com>
2021-02-28 19:22:41 -0300

    Tabela etapas adicionada

--- DB_init/academico.php
+++ DB_init/academico.php
@@ -5,7 +5,7 @@ include_once 'lib/libInitDB.php';
 
 // lista com o caminho dos arquivos sql a serem interpretados
 $sql_list = array(
-  'sql/B/campi', 'sql/B/cursos', 'sql/B/turmas'
+  'sql/B/campi', 'sql/B/cursos', 'sql/B/turmas', 'sql/I/etapas'
 );
 initDB($conn, $sql_list, 'academico', true);
 
