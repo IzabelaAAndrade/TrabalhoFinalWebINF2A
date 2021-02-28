a194b6ed241457612f57c4972caf0b4e6d53b046
Vinicius Lima <52431971+viniilima@users.noreply.github.com>
2021-02-28 19:23:43 -0300

    Tabela descartes adicionada

--- DB_init/biblioteca.php
+++ DB_init/biblioteca.php
@@ -4,7 +4,10 @@ include_once 'lib/libConnection.php';
 include_once 'lib/libInitDB.php';
 
 // lista com o caminho dos arquivos sql a serem interpretados
-$sql_list = array('sql/G/periodicos', 'sql/G/academicos', 'sql/G/midias');
+$sql_list = array(
+  'sql/G/periodicos', 'sql/G/academicos', 'sql/G/midias', 'sql/I/descartes'
+);
+
 initDB($conn, $sql_list, 'biblioteca', true);
 
 ?>
