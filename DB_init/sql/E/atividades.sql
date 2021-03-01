636e8e11f25c32848c437112a4d6098e3e6b79ce
MariaECarmona <51420077+MariaECarmona@users.noreply.github.com>
2021-02-28 22:39:42 -0300

    Update atividades.sql

--- DB_init/sql/E/atividades.sql
+++ DB_init/sql/E/atividades.sql
@@ -1,8 +1,7 @@
 CREATE TABLE IF NOT EXISTS atividades (
-  `id` int(11) NOT NULL,
-  `id_disciplinas` int(11) NOT NULL,
-  `nome` varchar(255) NOT NULL,
-  `data` date NOT NULL,
-  `valor` double NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-COMMIT;
+   id int NOT NULL,
+   id_disciplinas int NOT NULL,
+   nome varchar(30) NOT NULL,
+   data date NOT NULL,
+   valor double NOT NULL
+)
