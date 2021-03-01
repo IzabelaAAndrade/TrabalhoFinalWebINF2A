d06c978262240be29e42d26cf7ea53b6b5f6cb7d
MariaECarmona <51420077+MariaECarmona@users.noreply.github.com>
2021-02-28 22:47:53 -0300

    Update etapas.sql

--- DB_init/sql/E/etapas.sql
+++ DB_init/sql/E/etapas.sql
@@ -1,38 +1,4 @@
--- phpMyAdmin SQL Dump
--- version 5.0.4
--- https://www.phpmyadmin.net/
---
--- Host: 127.0.0.1
--- Tempo de geração: 26-Fev-2021 às 16:05
--- Versão do servidor: 10.4.17-MariaDB
--- versão do PHP: 7.4.15
-
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-START TRANSACTION;
-SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
---
--- Banco de dados: `test`
---
-
--- --------------------------------------------------------
-
---
--- Estrutura da tabela `etapas_da`
---
-
 CREATE TABLE IF NOT EXISTS etapas (
-  `id_ORDEM` int(15) NOT NULL,
-  `valor` int(5) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-COMMIT;
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+  id_ORDEM int(15) NOT NULL,
+  valor int(5) NOT NULL
+)
