f905d2e3f1b8d2a7fe2fd5c3d3406abf6a314b24
MariaECarmona <51420077+MariaECarmona@users.noreply.github.com>
2021-02-28 22:48:20 -0300

    Update atividades.sql

--- DB_init/sql/E/atividades.sql
+++ DB_init/sql/E/atividades.sql
@@ -1,7 +1,7 @@
 CREATE TABLE IF NOT EXISTS atividades (
-   id int NOT NULL,
-   id_disciplinas int NOT NULL,
-   nome varchar(30) NOT NULL,
-   data date NOT NULL,
-   valor double NOT NULL
+  id int NOT NULL,
+  id_disciplinas int NOT NULL,
+  nome varchar(30) NOT NULL,
+  data date NOT NULL,
+  valor double NOT NULL
 )
