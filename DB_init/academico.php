b582c234030ed84266ca29aaf79caeb14a1dd1d4
Vinicius Lima <52431971+viniilima@users.noreply.github.com>
2021-03-01 07:54:29 -0300

    Update academico.php

--- DB_init/academico.php
+++ DB_init/academico.php
@@ -5,7 +5,7 @@ include_once 'lib/libInitDB.php';
 
 // lista com o caminho dos arquivos sql a serem interpretados
 $sql_list = array(
-  'sql/B/campi', 'sql/B/cursos', 'sql/B/turmas', 'sql/E/atividades', 'sql/E/conteudos', 'sql/E/etapas', 'sql/I/etapas'
+  'sql/B/campi', 'sql/B/cursos', 'sql/B/turmas', 'sql/E/atividades', 'sql/E/conteudos', 'sql/E/etapas'
 );
 initDB($conn, $sql_list, 'academico', true);
 
