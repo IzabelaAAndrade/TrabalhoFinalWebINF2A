ddb9f121721e324e3f9ad7e3ae01057ee1f440fd
MariaECarmona <51420077+MariaECarmona@users.noreply.github.com>
2021-02-28 22:30:46 -0300

    Update atividades.sql

--- DB_init/sql/E/atividades.sql
+++ DB_init/sql/E/atividades.sql
@@ -1,32 +1,3 @@
--- phpMyAdmin SQL Dump
--- version 5.0.2
--- https://www.phpmyadmin.net/
---
--- Host: 127.0.0.1
--- Tempo de geração: 25-Fev-2021 às 14:35
--- Versão do servidor: 10.4.14-MariaDB
--- versão do PHP: 7.4.10
-
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-START TRANSACTION;
-SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
---
--- Banco de dados: `diario_academico`
---
-
--- --------------------------------------------------------
-
---
--- Estrutura da tabela `atividades`
---
-
 CREATE TABLE IF NOT EXISTS atividades (
   `id` int(11) NOT NULL,
   `id_disciplinas` int(11) NOT NULL,
@@ -35,7 +6,3 @@ CREATE TABLE IF NOT EXISTS atividades (
   `valor` double NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 COMMIT;
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
