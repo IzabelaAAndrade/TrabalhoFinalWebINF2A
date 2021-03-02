2edef2b7d4b913aaf6d22a230e5dc24a0ee8e861
MariaECarmona <51420077+MariaECarmona@users.noreply.github.com>
2021-03-02 10:49:45 -0300

    Update conteudos.sql

--- DB_init/sql/E/conteudos.sql
+++ DB_init/sql/E/conteudos.sql
@@ -1,7 +1,10 @@
+SET time_zone = 'America/Sao_Paulo';
+
 CREATE TABLE IF NOT EXISTS conteudos (
-  id bigint(20) NOT NULL,
-  id_etapa int(11) NOT NULL,
-  id_disciplinas int(11) NOT NULL,
-  conteudos varchar(30) CHARACTER SET latin1 COLLATE latin1_general_ci NOT NULL,
-  datas date NOT NULL
-)
+  `id` bigint(20) NOT NULL,
+  `id_etapas` int(11) NOT NULL,
+  `id_disciplinas` int(11) NOT NULL,
+  `conteudos` varchar(30) CHARACTER SET latin1 COLLATE latin1_general_ci NOT NULL,
+  `datas` date NOT NULL
+  PRIMARY KEY(`id`)
+);
