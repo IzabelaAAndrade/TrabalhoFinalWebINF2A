92aadf358f4127fa146f5d146faa437b90310625
MariaECarmona <51420077+MariaECarmona@users.noreply.github.com>
2021-03-02 10:34:36 -0300

    Update atividades.sql

--- DB_init/sql/E/atividades.sql
+++ DB_init/sql/E/atividades.sql
@@ -1,7 +1,11 @@
-CREATE TABLE IF NOT EXISTS atividades (
-  id int NOT NULL,
-  id_disciplinas int NOT NULL,
-  nome varchar(30) NOT NULL,
-  data date NOT NULL,
-  valor double NOT NULL
-)
+SET time_zone = 'America/Sao_Paulo';
+
+CREATE TABLE IF NOT EXISTS `atividades` (
+  `id` int(11) NOT NULL,
+  `id_disciplinas` int(11) NOT NULL,
+  `nome` varchar(255) NOT NULL,
+  `data` date NOT NULL,
+  `valor` double NOT NULL,
+  PRIMARY KEY(`id`)
+);
+
