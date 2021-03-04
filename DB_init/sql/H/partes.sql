f96538e1b0c8f9aa4955fcf2031ca9a1ecafa3fa
Joao-Queiroga <79543016+Joao-Queiroga@users.noreply.github.com>
2021-03-04 18:20:32 -0300

    Create partes.sql

---
+++ DB_init/sql/H/partes.sql
@@ -0,0 +1,10 @@
+DROP TABLE IF EXISTS partes;
+CREATE TABLE IF NOT EXISTS partes(
+    Id int(4) UNSIGNED NOT NULL AUTO_INCREMENT,
+    Id_periodicos int(4) UNSIGNED NOT NULL,
+    Titulo varchar(40) NOT NULL,
+    Pag_inicio int(4) NOT NULL,
+    Pag_final  int(4) NOT NULL,
+    Palavras_chave varchar(20),
+    PRIMARY KEY(Id)
+);
