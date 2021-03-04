74d059075984852d8fa25e8c94ccdfa54b141591
Izabela A. Andrade (bela) <51242342+IzabelaAAndrade@users.noreply.github.com>
2021-03-04 20:25:52 -0300

    Arquivos Contendo os Bancos de Dados Completos

---
+++ Bancos_de_Dados/Academico.sql
@@ -0,0 +1,315 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Tempo de geração: 05-Mar-2021 às 03:23
+-- Versão do servidor: 10.4.11-MariaDB
+-- versão do PHP: 7.4.3
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Banco de dados: `academico`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `alunos`
+--
+
+CREATE TABLE `alunos` (
+  `id` varchar(15) NOT NULL,
+  `nome` varchar(50) NOT NULL,
+  `sexo` varchar(10) DEFAULT NULL,
+  `nascimento` date DEFAULT NULL,
+  `logradouro` varchar(15) DEFAULT NULL,
+  `numero` smallint(6) DEFAULT NULL,
+  `complemento` varchar(15) DEFAULT NULL,
+  `bairro` varchar(15) DEFAULT NULL,
+  `cidade` varchar(15) DEFAULT NULL,
+  `cep` varchar(15) DEFAULT NULL,
+  `uf` varchar(2) DEFAULT NULL,
+  `email` varchar(50) DEFAULT NULL,
+  `foto` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `atividades`
+--
+
+CREATE TABLE `atividades` (
+  `id` int(11) NOT NULL,
+  `id_disciplinas` int(11) NOT NULL,
+  `nome` varchar(255) NOT NULL,
+  `data` date NOT NULL,
+  `valor` double NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `campi`
+--
+
+CREATE TABLE `campi` (
+  `id` int(11) NOT NULL,
+  `nome` varchar(255) NOT NULL,
+  `cidade` varchar(255) NOT NULL,
+  `uf` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `conteudos`
+--
+
+CREATE TABLE `conteudos` (
+  `id` bigint(20) NOT NULL,
+  `id_etapas` int(11) NOT NULL,
+  `id_disciplinas` int(11) NOT NULL,
+  `conteudos` varchar(30) CHARACTER SET latin1 COLLATE latin1_general_ci NOT NULL,
+  `datas` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `cursos`
+--
+
+CREATE TABLE `cursos` (
+  `id` int(11) NOT NULL,
+  `id_depto` varchar(255) NOT NULL,
+  `nome` varchar(255) NOT NULL,
+  `horas_total` int(11) NOT NULL,
+  `modalidade` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `depto`
+--
+
+CREATE TABLE `depto` (
+  `id` int(11) NOT NULL,
+  `id_campi` int(11) NOT NULL,
+  `nome` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `diario`
+--
+
+CREATE TABLE `diario` (
+  `id_conteudos` int(11) NOT NULL,
+  `id_matriculas` int(11) NOT NULL,
+  `id_atividades` int(11) NOT NULL,
+  `faltas` int(11) NOT NULL,
+  `nota` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `disciplinas`
+--
+
+CREATE TABLE `disciplinas` (
+  `id` int(8) NOT NULL,
+  `id_turmas` int(8) NOT NULL,
+  `nome` varchar(60) NOT NULL,
+  `carga_horaria_min` decimal(8,0) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `etapas`
+--
+
+CREATE TABLE `etapas` (
+  `id` int(15) NOT NULL,
+  `valor` int(5) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `matriculas`
+--
+
+CREATE TABLE `matriculas` (
+  `id` int(8) NOT NULL,
+  `id_alunos` int(8) NOT NULL,
+  `id_disciplinas` int(8) NOT NULL,
+  `ano` char(4) NOT NULL,
+  `ativo` varchar(2) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `professores`
+--
+
+CREATE TABLE `professores` (
+  `id` varchar(7) NOT NULL,
+  `id_depto` int(11) NOT NULL,
+  `nome` varchar(255) NOT NULL,
+  `titulacao` varchar(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `prof_disciplinas`
+--
+
+CREATE TABLE `prof_disciplinas` (
+  `Id-Professores` int(8) NOT NULL,
+  `Id-Disciplinas` int(8) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `turmas`
+--
+
+CREATE TABLE `turmas` (
+  `id` int(11) NOT NULL,
+  `id_cursos` int(11) NOT NULL,
+  `nome` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Índices para tabelas despejadas
+--
+
+--
+-- Índices para tabela `alunos`
+--
+ALTER TABLE `alunos`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Índices para tabela `atividades`
+--
+ALTER TABLE `atividades`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Índices para tabela `campi`
+--
+ALTER TABLE `campi`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Índices para tabela `conteudos`
+--
+ALTER TABLE `conteudos`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Índices para tabela `cursos`
+--
+ALTER TABLE `cursos`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Índices para tabela `depto`
+--
+ALTER TABLE `depto`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Índices para tabela `disciplinas`
+--
+ALTER TABLE `disciplinas`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Índices para tabela `etapas`
+--
+ALTER TABLE `etapas`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Índices para tabela `matriculas`
+--
+ALTER TABLE `matriculas`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Índices para tabela `professores`
+--
+ALTER TABLE `professores`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Índices para tabela `turmas`
+--
+ALTER TABLE `turmas`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT de tabelas despejadas
+--
+
+--
+-- AUTO_INCREMENT de tabela `campi`
+--
+ALTER TABLE `campi`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de tabela `cursos`
+--
+ALTER TABLE `cursos`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de tabela `depto`
+--
+ALTER TABLE `depto`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de tabela `disciplinas`
+--
+ALTER TABLE `disciplinas`
+  MODIFY `id` int(8) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de tabela `matriculas`
+--
+ALTER TABLE `matriculas`
+  MODIFY `id` int(8) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de tabela `turmas`
+--
+ALTER TABLE `turmas`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
---
+++ Bancos_de_Dados/Biblioteca.sql
@@ -0,0 +1,257 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Tempo de geração: 05-Mar-2021 às 03:20
+-- Versão do servidor: 10.4.11-MariaDB
+-- versão do PHP: 7.4.3
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Banco de dados: `biblioteca`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `academicos`
+--
+
+CREATE TABLE `academicos` (
+  `id_obra` int(11) NOT NULL,
+  `id_acervo` int(11) NOT NULL,
+  `programa` varchar(30) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `acervo`
+--
+
+CREATE TABLE `acervo` (
+  `id` int(11) NOT NULL,
+  `id_campi` int(11) NOT NULL,
+  `nome` varchar(255) NOT NULL,
+  `local` varchar(255) NOT NULL,
+  `tipo` enum('livros','periodicos','academicos','midias') NOT NULL,
+  `ano` int(11) NOT NULL,
+  `editora` varchar(255) NOT NULL,
+  `paginas` int(11) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `alunos`
+--
+
+CREATE TABLE `alunos` (
+  `id` varchar(15) NOT NULL,
+  `nome` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `autores`
+--
+
+CREATE TABLE `autores` (
+  `Id_obra` int(4) UNSIGNED NOT NULL,
+  `Id_autor` int(4) UNSIGNED NOT NULL,
+  `Nomes` varchar(40) NOT NULL,
+  `Sobrenomes` varchar(40) NOT NULL,
+  `Ordem` varchar(20) NOT NULL,
+  `Qualificacao` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `descartes`
+--
+
+CREATE TABLE `descartes` (
+  `id_acervo` int(11) NOT NULL,
+  `data_descarte` date NOT NULL,
+  `motivos` varchar(200) NOT NULL,
+  `operador` varchar(30) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `emprestimos`
+--
+
+CREATE TABLE `emprestimos` (
+  `Id` int(4) UNSIGNED NOT NULL,
+  `Id_alunos` int(4) UNSIGNED NOT NULL,
+  `Id_acervo` int(4) UNSIGNED NOT NULL,
+  `Data_emprestimo` date NOT NULL,
+  `Data_prev_devol` date NOT NULL,
+  `Data_devolucao` date DEFAULT NULL,
+  `Multa` double DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `livros`
+--
+
+CREATE TABLE `livros` (
+  `id_obra` int(11) NOT NULL,
+  `id_acervo` int(11) NOT NULL,
+  `edicao` varchar(200) NOT NULL,
+  `isbn` varchar(200) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `midias`
+--
+
+CREATE TABLE `midias` (
+  `id_obra` int(11) NOT NULL,
+  `id_acervo` int(11) NOT NULL,
+  `tempo` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
+  `subtipo` enum('CD','DVD','Fita','PenDrive') NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `partes`
+--
+
+CREATE TABLE `partes` (
+  `Id` int(4) UNSIGNED NOT NULL,
+  `Id_periodicos` int(4) UNSIGNED NOT NULL,
+  `Titulo` varchar(40) NOT NULL,
+  `Pag_inicio` int(4) NOT NULL,
+  `Pag_final` int(4) NOT NULL,
+  `Palavras_chave` varchar(20) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `periodicos`
+--
+
+CREATE TABLE `periodicos` (
+  `id` int(11) NOT NULL,
+  `periodicidade` enum('diario','mensal','anual','indefinido') NOT NULL,
+  `mes` enum('jan','feb','mar','abr','mai','jun','jul','ago','sep','out','nov','dez') NOT NULL,
+  `volume` int(11) NOT NULL,
+  `subtipo` enum('revista','jornal','boletim') NOT NULL,
+  `issn` varchar(8) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `reservas`
+--
+
+CREATE TABLE `reservas` (
+  `id` int(11) NOT NULL,
+  `id_alunos` int(11) NOT NULL,
+  `id_acervo` int(11) NOT NULL,
+  `data_reserva` date NOT NULL,
+  `tempo_espera` int(11) NOT NULL,
+  `emprestou` char(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Índices para tabelas despejadas
+--
+
+--
+-- Índices para tabela `alunos`
+--
+ALTER TABLE `alunos`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Índices para tabela `autores`
+--
+ALTER TABLE `autores`
+  ADD PRIMARY KEY (`Id_autor`);
+
+--
+-- Índices para tabela `emprestimos`
+--
+ALTER TABLE `emprestimos`
+  ADD PRIMARY KEY (`Id`);
+
+--
+-- Índices para tabela `partes`
+--
+ALTER TABLE `partes`
+  ADD PRIMARY KEY (`Id`);
+
+--
+-- Índices para tabela `periodicos`
+--
+ALTER TABLE `periodicos`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Índices para tabela `reservas`
+--
+ALTER TABLE `reservas`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT de tabelas despejadas
+--
+
+--
+-- AUTO_INCREMENT de tabela `autores`
+--
+ALTER TABLE `autores`
+  MODIFY `Id_autor` int(4) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de tabela `emprestimos`
+--
+ALTER TABLE `emprestimos`
+  MODIFY `Id` int(4) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de tabela `partes`
+--
+ALTER TABLE `partes`
+  MODIFY `Id` int(4) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de tabela `periodicos`
+--
+ALTER TABLE `periodicos`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de tabela `reservas`
+--
+ALTER TABLE `reservas`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
