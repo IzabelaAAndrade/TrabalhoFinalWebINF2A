a25e73a64793c6f3c6accb0e79355ea551b66790
Izabela A. Andrade (bela) <51242342+IzabelaAAndrade@users.noreply.github.com>
2021-03-04 20:30:22 -0300

    Adição das Tabelas que Faltavam

--- DB_init/biblioteca.php
+++ DB_init/biblioteca.php
@@ -5,8 +5,8 @@ include_once 'lib/libInitDB.php';
 
 // lista com o caminho dos arquivos sql a serem interpretados
 $sql_list = array(
-  'sql/A/alunos.sql','sql/G/periodicos', 'sql/G/academicos', 'sql/G/midias', 'sql/I/descartes', 'sql/I/reservas', 'sql/F/acervo.sql', 
-  'sql/F/livros.sql', 'sql/H/autores.sql', 'sql/H/emprestimos.sql','sql/H/partes.sql' 
+  'sql/A/alunos.sql','sql/G/periodicos.sql', 'sql/G/academicos.sql', 'sql/G/midias.sql', 'sql/I/descartes.sql', 'sql/I/reservas.sql', 'sql/F/acervo.sql', 
+  'sql/F/livros.sql', 'sql/H/autores.sql', 'sql/H/emprestimos.sql','sql/H/partes.sql', 
 );
 
 initDB($conn, $sql_list, 'biblioteca', true);
