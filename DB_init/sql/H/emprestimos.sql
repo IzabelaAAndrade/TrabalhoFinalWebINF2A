a3d44f109d5936b49fdd7b5ce73fa54807a9fb23
Joao-Queiroga <79543016+Joao-Queiroga@users.noreply.github.com>
2021-03-04 18:22:00 -0300

    Update emprestimos.sql

--- DB_init/sql/H/emprestimos.sql
+++ DB_init/sql/H/emprestimos.sql
@@ -3,7 +3,6 @@ CREATE TABLE IF NOT EXISTS emprestimos(
     Id int(4) UNSIGNED NOT NULL AUTO_INCREMENT,
     Id_alunos int(4) UNSIGNED NOT NULL,
     Id_acervo int(4) UNSIGNED NOT NULL,
-    Data_reserva DATE NOT NULL,
     Data_emprestimo DATE NOT NULL,
     Data_prev_devol DATE NOT NULL ,
     Data_devolucao DATE,
