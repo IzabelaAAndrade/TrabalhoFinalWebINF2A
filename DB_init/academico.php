db5386f6688543d9e16adf51f337f43603b22461
Izabela A. Andrade (bela) <51242342+IzabelaAAndrade@users.noreply.github.com>
2021-03-04 19:42:45 -0300

    Adição de Tabelas Faltantes

--- DB_init/academico.php
+++ DB_init/academico.php
@@ -5,7 +5,9 @@ include_once 'lib/libInitDB.php';
 
 // lista com o caminho dos arquivos sql a serem interpretados
 $sql_list = array(
-  'sql/B/campi', 'sql/B/cursos', 'sql/B/turmas', 'sql/E/atividades', 'sql/E/conteudos', 'sql/E/etapas'
+  'sql/B/campi', 'sql/B/cursos', 'sql/B/turmas', 'sql/E/atividades', 'sql/E/conteudos', 'sql/E/etapas', 
+  'sql/C/depto.sql', 'sql/C/professores.sql', 'sql/D/disciplinas.sql', 'sql/D/matriculas.sql',
+  'sql/D/prof_disciplinas.sql','sql/F/diario.sql', 'sql/C/alunos.sql'
 );
 initDB($conn, $sql_list, 'academico', true);
 
