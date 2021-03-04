7582b4342bed800a5e0d7df6b3bd1d0f050a58e5
Joao-Queiroga <79543016+Joao-Queiroga@users.noreply.github.com>
2021-03-04 18:19:19 -0300

    Create emprestimos.sql

---
+++ DB_init/sql/H/emprestimos.sql
@@ -0,0 +1,12 @@
+DROP TABLE IF EXISTS emprestimos;
+CREATE TABLE IF NOT EXISTS emprestimos(
+    Id int(4) UNSIGNED NOT NULL AUTO_INCREMENT,
+    Id_alunos int(4) UNSIGNED NOT NULL,
+    Id_acervo int(4) UNSIGNED NOT NULL,
+    Data_reserva DATE NOT NULL,
+    Data_emprestimo DATE NOT NULL,
+    Data_prev_devol DATE NOT NULL ,
+    Data_devolucao DATE,
+    Multa double,
+    PRIMARY KEY (Id)
+);
