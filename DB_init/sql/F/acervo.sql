1bbe680737d65b53e445d7633027a60057990de3
Tássyla Lima <56099401+tatitassyla@users.noreply.github.com>
2021-03-04 13:44:43 -0300

    Atualização das tabelas

--- DB_init/sql/F/acervo.sql
+++ DB_init/sql/F/acervo.sql
@@ -1,32 +1,3 @@
--- phpMyAdmin SQL Dump
--- version 5.0.2
--- https://www.phpmyadmin.net/
---
--- Host: localhost
--- Tempo de geração: 27-Fev-2021 às 17:55
--- Versão do servidor: 10.4.13-MariaDB
--- versão do PHP: 7.4.8
-
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-START TRANSACTION;
-SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
---
--- Banco de dados: `Banco`
---
-
--- --------------------------------------------------------
-
---
--- Estrutura da tabela `acervo`
---
-
 CREATE TABLE IF NOT EXISTS `acervo` (
   `id` int(11) NOT NULL,
   `id_campi` int(11) NOT NULL,
@@ -37,28 +8,3 @@ CREATE TABLE IF NOT EXISTS `acervo` (
   `editora` varchar(255) NOT NULL,
   `paginas` int(11) NOT NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4;
-
---
--- Índices para tabelas despejadas
---
-
---
--- Índices para tabela `acervo`
---
-ALTER TABLE `acervo`
-  ADD PRIMARY KEY (`id`);
-
---
--- AUTO_INCREMENT de tabelas despejadas
---
-
---
--- AUTO_INCREMENT de tabela `acervo`
---
-ALTER TABLE `acervo`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
-COMMIT;
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
