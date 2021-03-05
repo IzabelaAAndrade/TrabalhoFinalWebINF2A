f1228e6af9a90b5781b266be3af2077519600ab7
Gabriel Gonçalves <63806035+gabriel-py@users.noreply.github.com>
2021-03-05 20:54:47 -0300

    Update matriculas.sql

--- DB_init/sql/D/matriculas.sql
+++ DB_init/sql/D/matriculas.sql
@@ -1,6 +1,6 @@
 CREATE TABLE `matriculas` (
   `id` int(8) PRIMARY KEY AUTO_INCREMENT NOT NULL,
-  `id_alunos` int(8) NOT NULL,
+  `id_alunos` varchar(9) NOT NULL,
   `id_disciplinas` int(8) NOT NULL,
   `ano` char(4) NOT NULL,
   `ativo` varchar(2) NOT NULL
