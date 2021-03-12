514136166c17a85a7f4f9b633d37a8e445ff0331
Joao-Queiroga <79543016+Joao-Queiroga@users.noreply.github.com>
2021-03-12 16:57:16 -0300

    Update relacao_acervo.php

--- biblioteca/menu_relatorios/relacao_acervo.php
+++ biblioteca/menu_relatorios/relacao_acervo.php
@@ -1,45 +1,30 @@
 <?php
 $cnx = mysqli_connect("localhost","root","123456","trabalho_biblioteca") or die("Erro de conexão!");
 $tipo_acervo = $_GET['acervo'];
-$titulo = ucfirst($tipo_acervo);
 $sql = "SELECT * FROM trabalho_biblioteca.acervo where tipo='$tipo_acervo'";
 $result = mysqli_query($cnx, $sql) or die("Erro de sql");
 $table = mysqli_fetch_all($result, MYSQLI_ASSOC);
 mysqli_close($cnx);
-print_r($table);
 ?>
-<!DOCTYPE html>
-<html lang="pt">
-<head>
-    <meta charset="UTF-8">
-    <title>Relatório do Acervo</title>
-    <style>
-
-    </style>
-</head>
-<body>
-    <h1>Relatório de <?=$titulo?> do Acervo</h1>
-    <table border="5">
-        <tr>
-            <th>ID</th>
-            <th>Nome</th>
-            <th>Local</th>
-            <th>Ano</th>
-            <th>Editora</th>
-            <th>Paginas</th>
-        </tr>
+<table border="5">
+    <tr>
+        <th>ID</th>
+        <th>Nome</th>
+        <th>Local</th>
+        <th>Ano</th>
+        <th>Editora</th>
+        <th>Paginas</th>
+    </tr>
 <?php
 for ($i = 0; $i < sizeof($table); $i++){
         echo "
-        <tr>
+    <tr>
         <td>".$table[$i]["id"]."</td>
         <td>".$table[$i]["nome"]."</td>
         <td>".$table[$i]["local"]."</td>
         <td>".$table[$i]["ano"]."</td>
         <td>".$table[$i]["editora"]."</td>
         <td>".$table[$i]["paginas"]."</td>
-        </tr>";
+    </tr>";
 }?>
-    </table>
-</body>
-</html>
+</table>
