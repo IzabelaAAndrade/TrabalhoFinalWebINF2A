a1f316315e0098e02c2cdf3752afce82f405db28
MariaECarmona <dudacarmona1802@gmil.com>
2021-03-13 16:31:22 -0300

    Update conect_bd.php

--- diario_academico/manutenção_turmas/conect_bd.php
+++ diario_academico/manutenção_turmas/conect_bd.php
@@ -1,4 +1,7 @@
 <?php
-$connection = mysqli_connect("localhost", "root", "", "test") or die("Falha na conexão");
-
+    $user = "root";
+    $password = "";
+    $database = "test";
+    $hostname = "localhost";
+    $connection = mysqli_connect($hostname, $user, $password,$database) or die ("Falha na conexão."); 
 ?>
