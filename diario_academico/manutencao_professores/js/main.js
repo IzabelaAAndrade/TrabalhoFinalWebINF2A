6539726e85e94380811861040e899a9fab6445a7
Pedro Simões <30507586+peo5@users.noreply.github.com>
2021-03-14 19:58:39 -0300

    commit inicial

---
+++ diario_academico/manutencao_professores/js/main.js
@@ -0,0 +1,111 @@
+let listaEl = document.getElementById('lista');
+let novoBtn = document.getElementById('novo');
+
+function Professor(id, nome='', titulo='') {
+    this.id = id;
+    this.nome = nome;
+    switch (titulo) {
+        case 'D':
+            titulo = 'Doutorado';
+            break;
+        case 'M':
+            titulo = 'Mestrado';
+            break;
+        case 'E':
+            titulo = 'Especialização';
+            break;
+        case 'G':
+            titulo = 'Graduação';
+            break;
+    }
+    this.titulo = titulo;
+}
+
+Professor.prototype.getDiv = function() {
+    let div = document.createElement('div');
+    div.classList.add('professor', 'solid');
+    
+    let nome = document.createElement('span');
+    nome.innerHTML = this.nome;
+    div.appendChild(nome);
+
+    let titulo = document.createElement('span');
+    titulo.innerHTML = this.titulo;
+    div.appendChild(titulo);
+
+    let editar = document.createElement('div');
+    editar.innerHTML = 'editar';
+    editar.addEventListener('click', () => showModal('modal/editar.php?id=' + this.id));
+    editar.classList.add('btn', 'solid');
+    div.appendChild(editar);
+
+    let deletar = document.createElement('div');
+    deletar.innerHTML = 'deletar';
+    deletar.addEventListener('click', () => showModal('modal/deletar.php?id=' + this.id));
+    deletar.classList.add('btn', 'solid');
+    div.appendChild(deletar);
+
+    return div;
+}
+
+function showModal(src) {
+    console.log('show modal ' + src);
+    open(src, '_self');
+}
+
+async function listProfs(parametros = '') {
+    // fetch professores
+    // criar professores
+    // adicionar divs à lista
+
+    let profs = await fetch('db/profsjson.php' + parametros).then(data => data.json());
+    listaEl.innerHTML = '';
+    for(prof of profs.lista) {
+        let profObj = new Professor(prof.id, prof.nome, prof.titulacao);
+        listaEl.appendChild(profObj.getDiv());
+    }
+}
+
+// cria o botão de criar alunos
+// function initAdd() {
+//     btn = document.createElement('div');
+//     btn.classList.add('btn');
+//     btn.innerHTML = 'criar professor';
+//     btn.addEventListener('click', () => showModal('modal/novo.php'));
+//     listaEl.appendChild(btn);
+// }
+
+function initFiltros() {
+    let filtrosEl = document.getElementById('filtro_container');
+
+    let filtros = [
+        new FiltroGenerico('nome', 'nome', ''),
+        new FiltroGenerico('depto', 'depto', ''),
+        new FiltroSelect('titulacao', 'titulacao', 'Graduação:G|Especialização:E|Mestrado:M|Doutorado:D')
+    ];
+
+    for(filtro of filtros)
+        filtrosEl.appendChild(filtro.getDiv());
+
+    let aplicar = document.getElementById('aplicar');
+    aplicar.addEventListener('click', () => {
+        let filtroParams = '';
+        for(filtro of filtros) {
+            if(filtroParams != '')
+                filtroParams += '&';
+            filtroParams += filtro.getUrl();
+        }
+        console.log('filtro params: ' + filtroParams);
+        listProfs('?' + filtroParams);
+    });
+
+    let reset = document.getElementById('reset');
+    reset.addEventListener('click', () => {
+        for(filtro of filtros)
+            filtro.reset();
+    });
+}
+
+novoBtn.addEventListener('click', () => showModal('modal/adicionar.html'));
+initFiltros();
+listProfs();
