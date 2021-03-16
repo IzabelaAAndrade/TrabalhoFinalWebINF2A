5eaa9ad7f75efd11633d8a8382df0482ab1ff06d
victorwo <33638656+victorwo@users.noreply.github.com>
2021-03-15 22:33:41 -0300

    Update exibir_turmas.php

--- diario_academico/manutenção_turmas/exibir_turmas.php
+++ diario_academico/manutenção_turmas/exibir_turmas.php
@@ -7,38 +7,35 @@
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
-     <title>Mostra turmas</title>
+    <title>Mostra turmas</title>
 </head>
 <body>
-
-<h1>Resultados da pesquisa</h1>
-
-<div>
-
 <?php
-include 'conect_bd.php';
-if(isset($_POST['submit_button'])){
-	$search = $_POST['busca'];
-	$sql = "SELECT * FROM turmas WHERE id LIKE '%$search%' OR nome LIKE '%$search%'";
-	$resultado = mysqli_query($connection, $sql);
-	$queryResultado = mysqli_num_rows($resultado);
-	echo " Existem " . $queryResultado . " resultados para sua pesquisa.<br>";
-	if($queryResultado > 0){
-		while($coluna = mysqli_fetch_array($resultado)) {
- echo "<div id='turma".$coluna['id']."'>";
+  include 'conect_bd.php';
+
+  $get = $_GET['id'];
+
+  $sql1 = "SELECT id, id_cursos, nome FROM turmas WHERE id_cursos = '".$_GET['id']."'";
+  $sql = "SELECT id, nome FROM cursos WHERE id = '".$_GET['id']."'";
+  $resultado = mysqli_query($connection, $sql);
+  $resultado1 = mysqli_query($connection, $sql1);
+
+  $coluna = mysqli_fetch_array($resultado);
+  echo  "<h1> Turmas do curso de ".$coluna["nome"]."</h1>" ;
+
+  if (mysqli_num_rows($resultado1) > 0) {
+    while($coluna = mysqli_fetch_array($resultado1)) {
+      echo "<div id='turma".$coluna['id']."'>";
       echo "<div><b>Nome da turma:</b> " . $coluna["nome"]."</div>";
       echo "<div><p> <b>Id</b>: " . $coluna["id"]."</p></div>";
       echo "<button id='btnEditar".$coluna['id']."' onclick='getIdTurma(".$coluna['id'].");' class='btnEditar btn btn-info btn-lg' data-toggle='modal' data-target='#alterarTurmaModal'>Editar</button></div><br>";
-		}
-	} else{
-		echo "";
+	  
 	}
-	
-}else{
-	echo "";
-}
+  } else {
+    echo "Existem 0 turmas para esse curso";
+  }
 
-mysqli_close($connection);
+  mysqli_close($connection);
 ?>
   <div class="modal fade" id="alterarTurmaModal" role="dialog">
     <div class="modal-dialog modal-lg">
@@ -73,17 +70,17 @@ mysqli_close($connection);
     <div class="modal-dialog modal-lg">
       <div class="modal-content">
         <div class="modal-header">
-            <button type="button" class="close" data-dismiss="modal" onclick="location.reload(true); ">&times;</button>
+            <button type="button" class="close" data-dismiss="modal" onclick="location.reload(true);">&times;</button>
             <h2>Criar nova turma</h2>
             <p>Preencha todos os dados </p>
             <p id="result1"></p>
         </div>
         <div class="modal-body">
-        <form name="form" id="formCriar"  method="POST" enctype="application/x-www-form-urlencoded;">
+        <form name="form" id="formCriar"  method="POST" enctype="application/x-www-form-urlencoded">
           <p>ID da turma:<input type="text" name="idTurma"></p>
           <p>Nome da Turma:<input type="text" name="nomeTurma"></p>
 		  <p>Id do curso:<input type="text" name="idCurso"></p>
-          <input type="submit" value="Confirmar" onclick="location.reload(true);">
+          <input type="submit" value="Confirmar">
         </form>
         </div>
         <div class="modal-footer">
