108407bb9f2acbc3bac788b8810e8022a8cc28c9
kroos <54759116+KroosBR@users.noreply.github.com>
2021-03-18 19:38:00 -0300

    Delete SistemaLogin directory

--- SistemaLogin/logout.php
+++
@@ -1,7 +0,0 @@
-<?php
-session_start();
-session_destroy();
-//unset($_SESSION['usuario']);
-header('Location: tela_login');
-exit();
-?>
--- SistemaLogin/verifica_login.php
+++
@@ -1,7 +0,0 @@
-<?php
-session_start();
-if(!$_SESSION['usuario']){
-    header('Location: tela_login.php');
-    exit();
-}
-?>
