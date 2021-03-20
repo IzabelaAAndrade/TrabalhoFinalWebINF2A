11338ffaa2f7877d781b9cc7baa0045629c4e9b1
Arthur71149 <51198520+Arthur71149@users.noreply.github.com>
2021-03-20 13:20:54 -0300

    Update atividade.php

--- diario_academico/atividades/atividade.php
+++ diario_academico/atividades/atividade.php
@@ -5,7 +5,7 @@
         <meta charset="UTF-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Manutenção de Campi</title>
+        <title>Manutenção de Atividades</title>
         <link rel="stylesheet" href="../../padrao_estilizacao/diario_academico/paginas_com_formularios/geral.css">
         <link rel="preconnect" href="https://fonts.gstatic.com">
         <link href="https://fonts.googleapis.com/css2?family=Nunito&display=swap" rel="stylesheet">
