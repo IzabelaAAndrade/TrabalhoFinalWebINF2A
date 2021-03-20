60eb93d6309339295055d83c72e04cead3b62ebd
kroos <54759116+KroosBR@users.noreply.github.com>
2021-03-19 21:25:42 -0300

    tava faltando essa virgula kk

--- DB_init/sql/E/conteudos.sql
+++ DB_init/sql/E/conteudos.sql
@@ -3,6 +3,6 @@ CREATE TABLE IF NOT EXISTS conteudos (
   `id_etapas` int(11) NOT NULL,
   `id_disciplinas` int(11) NOT NULL,
   `conteudos` varchar(30) CHARACTER SET latin1 COLLATE latin1_general_ci NOT NULL,
-  `datas` date NOT NULL
+  `datas` date NOT NULL,
   PRIMARY KEY(`id`)
 );
