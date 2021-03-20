e3a2fdacec9f0b63917485f98c65d0ccd7832aa7
Arthur71149 <51198520+Arthur71149@users.noreply.github.com>
2021-03-20 13:38:53 -0300

    Update conteudos.sql

--- DB_init/sql/E/conteudos.sql
+++ DB_init/sql/E/conteudos.sql
@@ -1,5 +1,5 @@
 CREATE TABLE `conteudos` (
-  `id` bigint(20) NOT NULL,
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
   `id_etapas` int(11) NOT NULL,
   `id_disciplinas` int(11) NOT NULL,
   `conteudos` varchar(30) CHARACTER SET latin1 COLLATE latin1_general_ci NOT NULL,
