506062be290e2e4606978a555033b1260edef943
kroos <54759116+KroosBR@users.noreply.github.com>
2021-03-21 16:09:54 -0300

    Update biblioteca.php

--- DB_init/arquivos_de_conexão/biblioteca.php
+++ DB_init/arquivos_de_conexão/biblioteca.php
@@ -1,12 +1,12 @@
 <?php
 
-include_once 'lib/libConnection.php';
-include_once 'lib/libInitDB.php';
-
+include_once '../lib/libConnection.php';
+include_once '../lib/libInitDB.php';
+global $conn;
 // lista com o caminho dos arquivos sql a serem interpretados
 $sql_list = array(
-  'sql/A/alunos.sql','sql/G/periodicos.sql', 'sql/G/academicos.sql', 'sql/G/midias.sql', 'sql/I/descartes.sql', 'sql/I/reservas.sql', 'sql/F/acervo.sql', 
-  'sql/F/livros.sql', 'sql/H/autores.sql', 'sql/H/emprestimos.sql','sql/H/partes.sql', 
+  '../sql/A/alunos.sql','../sql/G/periodicos.sql', '../sql/G/academicos.sql', '../sql/G/midias.sql', '../sql/I/descartes.sql', '../sql/I/reservas.sql', '../sql/F/acervo.sql',
+  '../sql/F/livros.sql', '../sql/H/autores.sql', '../sql/H/emprestimos.sql','../sql/H/partes.sql',
 );
 
 initDB($conn, $sql_list, 'biblioteca', true);
