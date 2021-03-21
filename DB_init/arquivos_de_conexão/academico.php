ac1dff4a457611d234283b8d0133e6a8adacfe46
kroos <54759116+KroosBR@users.noreply.github.com>
2021-03-21 16:09:31 -0300

    Update academico.php

--- DB_init/arquivos_de_conexão/academico.php
+++ DB_init/arquivos_de_conexão/academico.php
@@ -1,14 +1,16 @@
 <?php
 
-include_once 'lib/libConnection.php';
-include_once 'lib/libInitDB.php';
+include_once '../lib/libConnection.php';
+include_once '../lib/libInitDB.php';
 
+global $conn;
 // lista com o caminho dos arquivos sql a serem interpretados
 $sql_list = array(
-'sql/B/campi.sql', 'sql/B/cursos.sql', 'sql/B/turmas.sql', 'sql/E/atividades.sql', 'sql/E/conteudos.sql', 'sql/E/etapas.sql', 
-  'sql/C/depto.sql', 'sql/C/professores.sql', 'sql/D/disciplinas.sql', 'sql/D/matriculas.sql',
-   'sql/D/prof_disciplinas.sql','sql/F/diario.sql', 'sql/C/alunos.sql'
+'../sql/B/campi.sql', '../sql/B/cursos.sql', '../sql/B/turmas.sql', '../sql/E/atividades.sql', '../sql/E/conteudos.sql', '../sql/E/etapas.sql',
+  '../sql/C/depto.sql', '../sql/C/professores.sql', '../sql/D/disciplinas.sql', '../sql/D/matriculas.sql',
+   '../sql/D/prof_disciplinas.sql','../sql/F/diario.sql', '../sql/C/alunos.sql'
 );
 initDB($conn, $sql_list, 'academico', true);
 
 ?>
+
