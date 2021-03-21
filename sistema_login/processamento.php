1a68205da454e187d10986657bf584a053631ef6
kroos <54759116+KroosBR@users.noreply.github.com>
2021-03-21 16:36:33 -0300

    Update processamento.php

--- sistema_login/processamento.php
+++ sistema_login/processamento.php
@@ -19,7 +19,7 @@ $senha = mysqli_real_escape_string($conexao, $_POST['senha']);
 $query = "SELECT id_usuario, nome, email FROM usuario WHERE email = '" . $email . "' AND senha = md5('" . $senha . "')";
 $result = mysqli_query($conexao, $query);
 $row = mysqli_num_rows($result); // a quantidade de linhas da retornadas pela query
-
+mysqli_close($conexao);
 if ($row == 1) {
     $_SESSION['usuario'] = $email;
     header('Location: index.php'); // redirecio para a pagina principal
@@ -29,5 +29,5 @@ if ($row == 1) {
     header('Locatiion: tela_login.php');
     exit();
 }
-mysqli_close($conexao);
+
 ?>
