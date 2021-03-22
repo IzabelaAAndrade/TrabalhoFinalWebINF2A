4709efaa12ec92aef182e321aae2f1a2a7a4ecf7
Izabela A. Andrade (bela) <contatobelaaa@gmail.com>
2021-03-22 01:39:17 -0300

    Ajuste: Nome

--- sistema_login/processamento.php
+++ sistema_login/processamento.php
@@ -18,9 +18,11 @@ $senha = mysqli_real_escape_string($conexao, $_POST['senha']);
 $query = "SELECT id_usuario, nome, email FROM usuarios WHERE email = '$email' AND senha = md5('$senha')";
 $result = mysqli_query($conexao, $query);
 $row = mysqli_num_rows($result); //a quantidade de linhas da retornadas pela query
+$linha_busca =  mysqli_fetch_all($result);
 mysqli_close($conexao);
 if ($row == 1) {
     $_SESSION['usuario'] = $email;
+    $_SESSION['nome_user'] = $linha_busca[0][1]; //Criando a variável de sessão "nome", para aparecer na pág index
     header('Location: index.php'); // redirecio para a pagina principal
     exit();
 } else {
