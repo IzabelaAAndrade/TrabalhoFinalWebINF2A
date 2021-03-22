5087663db1f1d334bc090835221772e5459c1b2d
Joao-Queiroga <79543016+Joao-Queiroga@users.noreply.github.com>
2021-03-22 17:03:03 -0300

    Add files via upload

---
+++ diario_academico/menu_relatorios/relatorio_alunos.php
@@ -0,0 +1,49 @@
+<?php
+$cpf = $_GET["cpf"];
+$etapa = $_GET["etapa"];
+$mysqli = new mysqli("localhost", "root","123456", "trabalho_diario_academico");
+$aluno = getDados("SELECT * FROM alunos WHERE id=$cpf")[0];
+$matriculas = getDados("SELECT * FROM matriculas WHERE id_alunos=".$aluno['id']);
+foreach($matriculas as $matricula){
+    $conteudos[] = getDados("SELECT * FROM conteudos WHERE id_etapas=$etapa AND id_disciplinas=".$matricula["id_disciplinas"])[0];
+}
+foreach($conteudos as $conteudo){
+    $id_conteudo[] = $conteudo["id"];
+}
+$id_conteudo[] = 2;
+?>
+<!DOCTYPE html>
+<html lang="pt">
+<head>
+    <meta charset="UTF-8">
+    <title>Relatório de Alunos</title>
+</head>
+<body>
+<h1>Relatório de alunos</h1>
+<h2>Relatório Aluno <?= $aluno['nome']?></h2>
+<h3><?=$etapa?>ª Etapa</h3>
+<table>
+    <tr>
+        <th>Disciplina</th>
+        <th>Nota</th>
+    </tr>
+        <?php
+        foreach($matriculas as $matricula){
+            echo "<tr>";
+            $disciplina = getDados("SELECT nome FROM disciplinas WHERE id=".$matricula["id_disciplinas"])[0];
+            echo "<td>$disciplina[0]</td>";
+            $diario = getDados("SELECT * FROM diario WHERE id_matriculas=".$matricula["id"]." AND id_conteudos IN (".implode(",",$id_conteudo).")")[0];
+            echo "<td>$diario[4]</td>";
+            echo "</tr>";
+        }
+        ?>
+</table>
+</body>
+</html>
+<?php
+function getDados(String $sql): array
+{
+    global $mysqli;
+    $result = $mysqli->query($sql);
+    return mysqli_fetch_all($result, MYSQLI_BOTH);
+}
