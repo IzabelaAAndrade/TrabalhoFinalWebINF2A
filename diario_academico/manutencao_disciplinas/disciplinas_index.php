37cc9d671491aca30c1e064d1a4743e7b6d9de37
kroos <54759116+KroosBR@users.noreply.github.com>
2021-03-23 09:33:34 -0300

    Update disciplinas_index.php

--- diario_academico/manutencao_disciplinas/disciplinas_index.php
+++ diario_academico/manutencao_disciplinas/disciplinas_index.php
@@ -8,7 +8,7 @@ include_once "povoa_base.php";
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <title>Manutenção Disciplinas</title>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
     <link href="https://fonts.googleapis.com/css2?family=Nunito&display=swap" rel="stylesheet">
     <link rel="preconnect" href="https://fonts.gstatic.com">
