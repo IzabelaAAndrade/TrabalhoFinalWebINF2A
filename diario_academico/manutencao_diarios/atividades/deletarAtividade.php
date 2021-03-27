a9739e474de5c78a609441e71562c73bfd0fcfea
Arthur71149 <51198520+Arthur71149@users.noreply.github.com>
2021-03-27 18:45:41 -0300

    Add files via upload

--- diario_academico/manutencao_diarios/atividades/deletarAtividade.php
+++ diario_academico/manutencao_diarios/atividades/deletarAtividade.php
@@ -10,7 +10,14 @@ $dbname="academico";
 $con = mysqli_connect($dbhost,$dbuser,$dbpass) or die(mysql_error());
 $sel = mysqli_select_db($con,$dbname);
 
+$verif=true;
 $id=$_POST['id'];
-mysqli_query($con,"DELETE FROM atividades WHERE id='$id'");
+$sql=mysqli_query($con,"SELECT * FROM diario WHERE id_atividades='$id'");
+if (mysqli_num_rows($sql) > 0) {
+  echo "<script>alert('Não é possível deletar essa atividade');</script>";
+}
+else{
+  mysqli_query($con,"DELETE FROM atividades WHERE id='$id'");
+}
 echo "<script>location.href='atividade.php'</script>";
 ?>
