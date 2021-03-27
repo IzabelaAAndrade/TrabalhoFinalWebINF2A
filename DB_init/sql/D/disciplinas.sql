f4aa7a3522d7959956c4e615001086f1c347272e
viniilima <vinii.lima@outlook.com>
2021-03-27 19:32:29 -0300

    Link transferencia alunos

--- DB_init/sql/D/disciplinas.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE `disciplinas` (
-  `id` int(8) PRIMARY KEY AUTO_INCREMENT NOT NULL,
-  `id_turmas` int(8) NOT NULL,
-  `nome` varchar(60) NOT NULL,
-  `carga_horaria_min` decimal(8,0) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
--- DB_init/sql/E/etapas.sql
+++
@@ -1,5 +0,0 @@
-CREATE TABLE IF NOT EXISTS `etapas` (
-  `id` int(15) NOT NULL,
-  `valor` int(5) NOT NULL,
-  PRIMARY KEY(`id`)
-);
