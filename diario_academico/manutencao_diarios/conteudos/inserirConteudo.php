ddc81bc43406d4a9e15c657dcf8b14c3d3f4180e
Arthur71149 <51198520+Arthur71149@users.noreply.github.com>
2021-03-27 18:47:02 -0300

    Add files via upload

--- diario_academico/manutencao_diarios/conteudos/inserirConteudo.php
+++ diario_academico/manutencao_diarios/conteudos/inserirConteudo.php
@@ -11,11 +11,33 @@ $con = mysqli_connect($dbhost,$dbuser,$dbpass) or die(mysql_error());
 $sel = mysqli_select_db($con,$dbname);
 
 $id_etapa=$_POST['etapa'];
-$id_disciplina=$_POST['disciplina'];
+$id_disciplinas=$_POST['disciplina'];
 $conteudo=$_POST['conteudo'];
 $data_inicio=$_POST['data_inicio'];
 $data_final=$_POST['data_final'];
 $data = date('d-m-Y', strtotime(str_replace('-','/', $data_inicio)))." à ".date('d-m-Y', strtotime(str_replace('-','/', $data_final)));
-mysqli_query($con,"INSERT INTO conteudos(id_etapas,id_disciplinas,conteudos,datas) VALUES ('$id_etapa','$id_disciplina','$conteudo','$data')");
+$verif = true;
+
+$sql=mysqli_query($con,"SELECT * FROM disciplinas WHERE id='$id_disciplinas'");
+if (mysqli_num_rows($sql) > 0) {
+  $sql=mysqli_query($con,"SELECT * FROM conteudos WHERE id_disciplinas='$id_disciplinas'");
+  while($ln = mysqli_fetch_assoc($sql)){
+    if($id_etapa==$ln['id_etapas'] && strcmp($conteudo,$ln['conteudos'])==0 && strcmp($data,$ln['datas'])==0  ){
+      $verif=false;
+      echo "<script>alert('Não é possível inserir esse conteúdo');</script>";
+      echo "<script>location.href='conteudo.php'</script>";
+
+    }
+  }
+}
+else{
+  echo "<script>alert('Não é possível inserir esse conteúdo');</script>";
+  echo "<script>location.href='conteudo.php'</script>";
+  $verif=false;
+}
+if($verif==true){
+  mysqli_query($con,"INSERT INTO conteudos(id_etapas,id_disciplinas,conteudos,datas) VALUES ('$id_etapa','$id_disciplinas','$conteudo','$data')");
+  echo "<script>location.href='conteudo.php'</script>";
+}
 echo "<script>location.href='conteudo.php'</script>";
 ?>
