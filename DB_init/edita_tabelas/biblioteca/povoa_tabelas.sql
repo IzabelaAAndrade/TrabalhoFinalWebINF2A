44e597df69732d76c0e3c09acb5c1665886acd2d
Izabela A. Andrade (bela) <51242342+IzabelaAAndrade@users.noreply.github.com>
2021-03-28 20:04:14 -0300

    Update povoa_tabelas.sql

--- DB_init/edita_tabelas/biblioteca/povoa_tabelas.sql
+++ DB_init/edita_tabelas/biblioteca/povoa_tabelas.sql
@@ -11,13 +11,3 @@ INSERT INTO `alunos` (`id`, `nome`, `email`)  VALUES
 (9, 'Citra Terranova', 'gerenciawebinf2a@gmail.com'),
 (10, 'Jace Wayland', 'gerenciawebinf2a@gmail.com');
 
-
-
-
--- Povoar Tabela Empréstimos --
-INSERT INTO `emprestimos` (`Id`, `Id_alunos`, `Id_acervo`, `Data_emprestimo`, `Data_prev_devol`, `Data_devolucao`, `Multa`) VALUES
-(1, 1, 1, '2021-04-04', '2021-04-11', '2021-03-22', 0),
-(2, 2, 2, '2021-04-04', '2021-04-11', '2021-03-11', 0),
-(3, 3, 9, '2021-03-21', '2021-03-21', '2021-03-21', 0),
-(4, 6, 3, '2021-03-28', '2021-04-04', '2021-04-04', 0),
-(5, 5, 8, '2021-03-28', '2021-04-04', '2021-04-04', 0);
