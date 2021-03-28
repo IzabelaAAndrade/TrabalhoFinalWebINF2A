cd3195f89e3ece3903465d8a3fe17933ae2fa1d1
Izabela A. Andrade (bela) <contatobelaaa@gmail.com>
2021-03-28 16:21:24 -0300

    Ajustes de Email

--- envio_de_emails/envia_email.php
+++ envio_de_emails/envia_email.php
@@ -8,198 +8,39 @@ use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\SMTP;
 use PHPMailer\PHPMailer\Exception;
 
-$mail = new PHPMailer(true);
+$configuracoes_email = new PHPMailer(true);
+
+$email = "email.html";
+
+//String com o conteúdo do Emai
+$conteudo_email =  file_get_contents($email);
+
 
 try {
-	$mail->SMTPDebug = SMTP::DEBUG_SERVER;
-	$mail->isSMTP();
-	$mail->Host = 'smtp.gmail.com';
-	$mail->SMTPAuth = true;
-	$mail->Username = 'gerenciawebinf2a@gmail.com';
-	$mail->Password = 'IsaacIzabelaMarcela';
-	$mail->Port = 587;
+	$configuracoes_email->SMTPDebug = SMTP::DEBUG_SERVER;
+	$configuracoes_email->isSMTP();
+	$configuracoes_email->Host = 'smtp.gmail.com';
+	$configuracoes_email->SMTPAuth = true;
+	$configuracoes_email->Username = 'gerenciawebinf2a@gmail.com';
+	$configuracoes_email->Password = 'IsaacIzabelaMarcela';
+	$configuracoes_email->Port = 587;
 
-	$mail->setFrom('gerenciawebinf2a@gmail.com');
-	$mail->addAddress('contatobelaaa@gmail.com');
+	$configuracoes_email->setFrom('gerenciawebinf2a@gmail.com');
+	$configuracoes_email->addAddress('tatitassyla@gmail.com');
+    $configuracoes_email->addAddress('mayara.mendes.mdc@gmail.com');
 
-	$mail->isHTML(true);
-	$mail->Subject = '[NÃO RESPONDA] Teste de Envio de Emails, Sistema SORA';
-	$mail->Body = 
-    "
-    <!DOCTYPE html>
-        <!-- HTML geral para páginas que sejam do tipo formulários -->
-        <html lang=\"en\">
-            <head>
-                <meta charset=\"UTF-8\">
-                <meta http-equiv=\"X-UA-Compatible\" content=\"IE=edge\">
-                <meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\">
-                <title>Manutenção Acervo</title>
-                <link rel=\"stylesheet\" href=\"email.css\">
-                <link rel=\"preconnect\" href=\"https://fonts.gstatic.com\">
-                <link href=\"https://fonts.googleapis.com/css2?family=Nunito&display=swap\" rel=\"stylesheet\">
-            </head>
-            <style>
-            /* CSS para estilizar páginas do tipo Formulário - Biblioteca */
-                html, body{
-                    margin: 0%;
-                }
-                header, footer{
-                    height: 5%;
-                    background-color: #264553;
-                    padding: 3%;
-                }
+	$configuracoes_email->isHTML(true);
+	$configuracoes_email->Subject = '[Email Gerado Automaticamente] Teste de Envio de Emails, Sistema SORA';
+	$configuracoes_email->Body = $conteudo_email;
+	$configuracoes_email->AltBody = '[Email Teste] Sistema SORA de acervo!';
 
-                h1, h3, p, input{
-                    font-family: 'Nunito', sans-serif;
-                    color: white;
-                    margin: 0%;
-                }
-
-                h1{
-                    font-size: 1em;
-                    text-align: center;
-                    margin-top: 2%;
-                }
-
-                h3{
-                    text-align: center;
-                    font-size: 0.7em;
-                }
-
-                .sub, .msg{
-                    color: #449d8f;
-                    margin: 5% 0% .5% 0%;
-                    font-size: 1em;
-                }
-
-                .principal{
-                    color: #e76f51;
-                    font-size: 1.5em;
-                    margin: 0% 0% .5% 0%;
-                }
-
-                .descricao{
-                    color: #264553;
-                    font-size: 1em;
-                    margin: 0% auto 1% auto;
-                    width: 60vw;
-                }
-
-                .descricao, .principal, .subRosa{
-                    text-align: center;
-                }
-
-                #ajuste{
-                    display: flex;
-                    align-items: center;
-                    justify-content: center;
-                    margin: 1% 0% 5% 0%;
-                }
-                form{
-                    display: block;
-
-                }
-
-                #botoes{
-                    display: block;
-                    margin-left: auto;
-                    margin-right: auto;
-                }
-
-                .botoes{
-                    width: 49.5%;
-                    height: 4vh;
-                    border-radius: .3em;
-                    border: 0;
-                    background-color: #e76f51;
-                    color: white;
-                    transition: .3s ease;
-                    
-                }
-                .botoes:hover{
-                    background-color: #449d8f;
-                    cursor:pointer
-                    
-                }
-
-                input{
-                    color:#449d8f;
-                    display: inline-block;
-                }
-
-
-
-                .texto{
-                    width: 30vw;
-                    border: 0;
-                    outline: none;
-                    border-bottom: 1.5px solid #449d8f ;
-                    display: block;
-                    margin: 0% 0% 5% 0%;
-                }
-
-                textarea{
-                    width: 100%;
-                    height: 10vh;
-                    padding: 3%;
-                    background-color: #449d8f;
-                    color: white;
-                    border: 0;
-                    box-sizing: border-box;
-                    font-family: 'Nunito', sans-serif;
-                }
-                textarea::placeholder{
-                    color: white;
-                }
-
-                .msg{
-                    margin: 1% 0% 5% 0%;
-                }
-                #logo{
-                    display: block;
-                    width: 20vh;
-                    margin-left: auto;
-                    margin-right: auto;
-                }
-
-            </style>
-            <body>
-                <header>
-                    <img src=\"img/LogoExemploCortada.png\" alt=\"logo\" id=\"logo\">
-                    <h1>Sistema de Biblioteca</h1>
-                </header>
-                <main>
-                    <h3 class=\"sub\">Infelizmente as notícias não são boas :(</h3>
-                    <h1 class=\"principal\">Sua Reserva foi cancelada</h1>
-                    <p class=\"descricao\">Olá [nome da pessoa]! Este email foi enviado para informá-lo(a) que sua Reserva
-                        do livro [nome do livro reservado] foi cancelada devido à exclusão do mesmo de  nosso acervo literário. 
-                        Nos desculpamos pelo incoveniente e o/a convidamos para reservar outra obra existente em nossa biblioteca. 
-                        
-                        Atenciosamente, 
-                        Equipe [nome do sistema]. 
-                        
-                    </p>
-                    <h3 class=\"msg\">Este email foi enviado automáticamente, favor não responder. </h3>
-
-                    </div>
-                </main>
-                <footer>
-                    <h3>Orgulhosamente criado pela turma de Informática 2A de ingresso em 2019©</h3>
-                    <h3>Agradeçemos pela preferência</h3>
-                </footer>
-
-            </body>
-        </html>
-        ";
-	$mail->AltBody = 'Chegou o email teste do Canal TI';
-
-	if($mail->send()) {
+	if($configuracoes_email->send()) {
 		echo 'Email enviado com sucesso';
 	} else {
 		echo 'Email nao enviado';
 	}
 } catch (Exception $e) {
-	echo "Erro ao enviar mensagem: {$mail->ErrorInfo}";
+	echo "Erro ao enviar mensagem: {$configuracoes_email->ErrorInfo}";
 }
 
 /*
@@ -226,7 +67,7 @@ try{ // Configurar o email
     $configuracao_email->Username = 'gerenciawebinf2a@gmail.com';
     $configuracao_email->Password = 'IsaacIzabelaMarcela';
     //Porta pela passagem do SMTP
-    $mail->Port = 587;
+    $configuracoes_email->Port = 587;
 
     //email propriamente dito
     $configuracao_email->setFrom('gerenciawebinf2a@gmail.com');
