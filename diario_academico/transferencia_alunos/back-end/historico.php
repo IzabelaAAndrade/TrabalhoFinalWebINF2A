dfefcc5d98d19b8e0dc32298faef78905e3f028b
Tássyla Lima <56099401+tatitassyla@users.noreply.github.com>
2021-03-29 00:45:16 -0300

    Update historico.php

--- diario_academico/transferencia_alunos/back-end/historico.php
+++ diario_academico/transferencia_alunos/back-end/historico.php
@@ -6,7 +6,7 @@ unset($_SESSION['historico']);
 $_SESSION['historico'] = '';
 
 //cpf do aluno
-$id = $_POST['cpf'];
+$id = $_SESSION['cpf'];
 
 //array que receberá os dados básicos do aluno - nome, cpf, média global e faltas
 $student_data =  [
