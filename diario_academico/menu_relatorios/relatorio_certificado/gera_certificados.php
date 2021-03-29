d158af250e2d6044372bbd9065b7acfc21a35d83
Gabriel Gonçalves <gabrielgoncalves310503@gmail.com>
2021-03-29 12:15:43 -0300

    .

--- diario_academico/menu_relatorios/relatorio_certificado/gera_certificados.php
+++ diario_academico/menu_relatorios/relatorio_certificado/gera_certificados.php
@@ -2,7 +2,7 @@
 
     require('conexao.php');
     $id_aluno = $_POST["id_aluno"];
-    $query = "SELECT * FROM alunos WHERE id=$id_aluno";
+    $query = "SELECT * FROM alunos WHERE id='$id_aluno'";
     $resultado_aluno = mysqli_query($conexao,$query) or die("<div class='alert alert-danger' role='alert'>Erro de conexão!</div>");
     $row_aluno = mysqli_fetch_array($resultado_aluno);
     $nome_aluno = $row_aluno["nome"];
@@ -19,38 +19,40 @@
                 <th>Faltas</th>
             </thead>";
     
-    $query = "SELECT * FROM matriculas WHERE id_alunos=$id_aluno";
+    $query = "SELECT * FROM matriculas WHERE id_alunos='$id_aluno'";
     $resultado_matriculas = mysqli_query($conexao,$query) or die("<div class='alert alert-danger' role='alert'>Erro de conexão!</div>");
+    $num_matriculas = mysqli_num_rows($resultado_matriculas);
+    if($num_matriculas>0){
+        while($row_matriculas = mysqli_fetch_array($resultado_matriculas)){
+            $id_matricula = $row_matriculas['id'];
+            $id_disciplina = $row_matriculas['id_disciplinas'];
+            echo "<tr><td>$id_matricula</td>";
+            
+            $query = "SELECT * FROM disciplinas WHERE id=$id_disciplina";
+            $resultado_disciplinas = mysqli_query($conexao,$query) or die("<div class='alert alert-danger' role='alert'>Erro de conexão!</div>");
+            $row_disciplinas = mysqli_fetch_array($resultado_disciplinas);
+            $nome_disciplina = $row_disciplinas['nome'];
+            $carga_horaria = $row_disciplinas['carga_horaria_min'];
+            echo "<td>$nome_disciplina</td>";
+            echo "<td>$carga_horaria</td>";
 
-    while($row_matriculas = mysqli_fetch_array($resultado_matriculas)){
-        $id_matricula = $row_matriculas['id'];
-        $id_disciplina = $row_matriculas['id_disciplinas'];
-        echo "<tr><td>$id_matricula</td>";
-        
-        $query = "SELECT * FROM disciplinas WHERE id=$id_disciplina";
-        $resultado_disciplinas = mysqli_query($conexao,$query) or die("<div class='alert alert-danger' role='alert'>Erro de conexão!</div>");
-        $row_disciplinas = mysqli_fetch_array($resultado_disciplinas);
-        $nome_disciplina = $row_disciplinas['nome'];
-        $carga_horaria = $row_disciplinas['carga_horaria_min'];
-        echo "<td>$nome_disciplina</td>";
-        echo "<td>$carga_horaria</td>";
-
-        $query = "SELECT * FROM diario WHERE id_matriculas=$id_matricula";
-        $resultado_diario = mysqli_query($conexao,$query) or die("<div class='alert alert-danger' role='alert'>Erro de conexão!</div>");
-        $num_diario = mysqli_num_rows($resultado_diario);
-        if($num_diario==1){
-            $row_diario = mysqli_fetch_array($resultado_diario);
-            $nota = $row_diario['nota'];
-            $faltas = $row_diario['faltas'];
-            echo "<td>$nota</td>";
-            echo "<td>$faltas</td></tr>";
-        } else {
-            echo "<td>Nota Indisponível.</td>";
-            echo "<td>Frequência Indisponível.</td></tr>";
+            $query = "SELECT * FROM diario WHERE id_matriculas=$id_matricula";
+            $resultado_diario = mysqli_query($conexao,$query) or die("<div class='alert alert-danger' role='alert'>Erro de conexão!</div>");
+            $num_diario = mysqli_num_rows($resultado_diario);
+            if($num_diario==1){
+                $row_diario = mysqli_fetch_array($resultado_diario);
+                $nota = $row_diario['nota'];
+                $faltas = $row_diario['faltas'];
+                echo "<td>$nota</td>";
+                echo "<td>$faltas</td></tr>";
+            } else {
+                echo "<td>Nota Indisponível.</td>";
+                echo "<td>Frequência Indisponível.</td></tr>";
+            }
         }
-        
-
     }
+
+    
 
     echo "</table>
     <div id='imprimir' class='solid btn imprimir'>Imprimir</div></div> 
