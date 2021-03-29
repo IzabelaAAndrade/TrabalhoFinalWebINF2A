92b163bb1220549ed7dde8bc0fb8dc2c26d16be2
Vinicius Lima <52431971+viniilima@users.noreply.github.com>
2021-03-28 21:27:30 -0300

    Create disciplina.sql

---
+++ DB_init/sql/D/disciplina.sql
@@ -0,0 +1,6 @@
+CREATE TABLE `disciplinas` (
+  `id` int(8) PRIMARY KEY AUTO_INCREMENT NOT NULL,
+  `id_turmas` int(8) NOT NULL,
+  `nome` varchar(60) NOT NULL,
+  `carga_horaria_min` decimal(8,0) NOT NULL
+)
