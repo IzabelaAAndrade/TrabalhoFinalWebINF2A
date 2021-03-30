cbcec6d567ef3c03de944ae64078d1f46c5ee676
Izabela A. Andrade (bela) <contatobelaaa@gmail.com>
2021-03-29 21:04:58 -0300

    Ajustes

--- diario_academico/sobre.php
+++ diario_academico/sobre.php
@@ -10,7 +10,7 @@ include '../sistema_login/verifica_login.php';
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>SORA | Sobre</title>
-        <link rel="stylesheet" href="index.css">
+        <link rel="stylesheet" href="index_diario.css">
         <link rel="stylesheet" href="sobre.css">
         <link rel="preconnect" href="https://fonts.gstatic.com">
         <link href="https://fonts.googleapis.com/css2?family=Nunito&display=swap" rel="stylesheet">
