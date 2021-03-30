a9d0e895834b1c9db78bcf545ad335afd5ef1331
Izabela A. Andrade (bela) <contatobelaaa@gmail.com>
2021-03-30 05:55:56 -0300

    Ajustes no CSS das Páginas Sobre

--- diario_academico/sobre.php
+++ diario_academico/sobre.php
@@ -21,7 +21,7 @@ include '../sistema_login/verifica_login.php';
 <body>
   <header>
     <img src="../imgs/sidaLogo.jpg" alt="logo" id="logo">
-    <h1 id="titulo">Sistema de Organização de Acervo</h1>
+    <h1 id="titulo">Sistema de Diário Acadêmico</h1>
     <div id="dados_user">
       <div id="aux">
         <h2 id="nome_user">Olá <?php echo $_SESSION['nome_user']; ?></h2>
@@ -30,24 +30,38 @@ include '../sistema_login/verifica_login.php';
     </div>
   </header>
   <nav>
-    <ul class="menu">
-      <li><a href="../sistema_login/index.php">Início</a></li>
-      <li><a href="index.php">Home</a></li>
-      <li><a href="sobre.php">Sobre</a></li>
-      <li><a href="#">Manutenção</a>
-        <ul class="sub_menu">
-          <li><a href="manutencao_acervo/index_acervo.php">Acervo</a></li>
-          <li><a href="manutenção_emprestimos/index.php">Empréstimos</a></li>
-        </ul>
-      </li>
-      <li><a href="reservas/cria_reserva.php">Reservas</a></li>
-      <li><a href="relatorios/index.php">Relatórios</a></li>
-
-    </ul>
-  </nav>
+            <ul class="menu">
+                <li><a href="../sistema_login/index.php">Início</a></li>
+	            <li><a href="index.php">Home</a></li>
+	            <li><a href="sobre.php">Sobre</a></li>
+	            <li><a href="#">Manutenção</a>
+	                <ul class="sub_menu">
+	                    <li><a href="campi/campi.php">Campi</a></li>
+	                    <li><a href="manutencao_departamentos/">Departamentos</a></li>
+	                    <li><a href="manutencao_cursos/">Cursos</a></li>
+	                    <li><a href="manutencao_turmas/">Turmas</a></li>
+	                    <li><a href="manutencao_alunos/">Alunos</a></li>
+	                    <li><a href="manutencao_professores/">Professores</a></li>
+	                    <li><a href="manutencao_disciplinas/disciplinas_index.php">Disciplinas</a></li>
+	                    <li><a href="manutencao_etapas/">Etapas</a></li>
+	                    <li><a href="manutencao_diarios/">Diários</a></li>
+	                </ul>
+	            </li>
+	            <li><a href="menu_relatorios/index.php">Relatórios</a>
+                    <ul class="sub_menu">
+                        <li><a href="menu_relatorios/relatorio_certificado/index.php">Certificados</a></li>
+                        <li><a href="menu_relatorios/relatorio_historico/index.php">Histórico por Aluno e Turma</a></li>
+                        <li><a href="menu_relatorios/relatorio_alunos/index_relatorio_aluno.php">Relação de Alunos</a></li>
+                        <li><a href="menu_relatorios/relatorio_relacao_conteudo/index.php">Relação de Conteúdos</a></li>
+                        <li><a href="menu_relatorios/relatorio_professores/index.php">Relação de Professores</a></li>
+                    </ul>
+                </li>
+	            <li><a href="transferencia_alunos/">Transferências</a></li>
+	        </ul>
+        </nav>
   <main>
     <h3 class="sub">Um pouco mais </h3>
-    <h1 class="principal">Sobre o SORA</h1>
+    <h1 class="principal">Sobre o SIDA</h1>
     <p id="esp" class="descricao">Hora de conhecer mais sobre este sistema e seus desenvolvedores</p>
 
     <h1> QUEM FAZ </H1>
