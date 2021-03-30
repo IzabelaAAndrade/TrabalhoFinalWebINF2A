8073bb82bdca529d56c622e7dc01af9912697d1b
Gabriel Coelho <51426003+gs-coelho@users.noreply.github.com>
2021-03-30 00:01:36 -0300

    Adiciona o grupo C à seção sobre da biblioteca

--- biblioteca/sobre.php
+++ biblioteca/sobre.php
@@ -165,31 +165,37 @@ include '../sistema_login/verifica_login.php';
                 <li data-target="#carouselExampleIndicatorsC" data-slide-to="2"></li>
             </ol>
             <div class="carousel-inner slidesC">
-                <div class="carousel-item active">
-                    <img src="../grupos_imgs/C/sua_img.png" class="imgSobre">
-                    <h3 class="sobreNome"> 👑 Gabriel Coelho dos Santos</h3>
-                    <div class="sobreCorpo">
-                        <p class="sobreParagrafos"> Seu texto
+            <div class="carousel-item active">
+                      <img src="../grupos_imgs/C/gabriel.jpg" class="imgSobre">
+                       <h3 class="sobreNome"> 👑 Gabriel Coelho dos Santos</h3>
+                       <div class="sobreCorpo">
+                          <p class="sobreParagrafos">Um programador viciado em música coreana e livros de fantasia, e que procrastina assistindo vídeos sobre linguística.
+                            <br>"A vida não é um quebra-cabeças que pode ser resolvido apenas uma vez e pronto. Você acorda todos os dias e o resolve novamente."
+                            <br>- Chidi Anagonye, 'The Good Place' 4x09
+                          </p>
+
+                        </div>
+                    </div>
+                    <div class="carousel-item slidesC">
+                        <img src="../grupos_imgs/C/giovana.jpg" class="imgSobre">
+                        <h3 class="sobreNome"> Giovana Caroba Bragio dos Santos  </h3>
+                        <div class="sobreCorpo">
+                            <p class="sobreParagrafos">"Happiness can be found, even in the darkest of times, if one only remembers to turn on the light."
+                              <br>- Albus Dumbledore
+                            </p>
+
+                        </div>
+                    </div>
+                    <div class="carousel-item slidesC">
+                       <img src="../grupos_imgs/C/joao.jpg" class="imgSobre">
+                       <h3 class="sobreNome"> João Vitor Lima Gomes</h3>
+                       <div class="sobreCorpo">
+                        <p class="sobreParagrafos">"We made these memories for ourselves."
+                          <br>- Ed Sheeran
                         </p>
 
+                      </div>
                     </div>
-                </div>
-                <div class="carousel-item slidesC">
-                    <img src="../grupos_imgs/C/sua_img.png" class="imgSobre">
-                    <h3 class="sobreNome"> Giovana Caroba Bragio dos Santos </h3>
-                    <div class="sobreCorpo">
-                        <p class="sobreParagrafos"> Seu Texto </p>
-
-                    </div>
-                </div>
-                <div class="carousel-item slidesC">
-                    <img src="../grupos_imgs/C/sua_img.png" class="imgSobre">
-                    <h3 class="sobreNome"> João Vitor Lima Gomes</h3>
-                    <div class="sobreCorpo">
-                        <p class="sobreParagrafos"> Seu Texto </p>
-
-                    </div>
-                </div>
 
             </div>
             <a class="carousel-control-prev" href="#carouselExampleIndicatorsC" role="button" data-slide="prev">
